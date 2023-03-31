0e1750c0de60d79e5e700bc33f5d01a45b4d2343
bemolina91 <bemolina9@icloud.com>
2023-03-31 12:00:26 -0500

    task 4 complete

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Reflection;
+
 namespace TechJobs.Tests
 {
 	[TestClass]
@@ -6,12 +8,36 @@ namespace TechJobs.Tests
 	{
         //Testing Objects
         //initalize your testing objects here
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+            new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+            new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            Assert.AreNotEqual(job1, job2, "Unique Id Test");
         }
+
+        [TestMethod] //4
+        public void TestJobConstructorSetsAllFields()
+        {
+            //verify 
+            Assert.AreEqual(job3.Name, "Product tester", "Testing Job Name");
+            Assert.AreEqual(job3.EmployerName.ToString(), "ACME", "Testing EmployerName Value");
+            Assert.AreEqual(job3.EmployerLocation.ToString(), "Desert", "Testing EmployerLocation Value");
+            Assert.AreEqual(job3.JobType.ToString(), "Quality control", "Testing JobType Value");
+            Assert.AreEqual(job3.JobCoreCompetency.ToString(), "Persistence", "Testing JobCoreCompetency Value");
+        }
+
+        [TestMethod] //5
+        public void TestJobsForEquality()
+        {
+            Assert.AreNotEqual(job3, job4, "Objects with different ID are not equal");
+        }
+
     }
 }
 
